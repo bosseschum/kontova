d3abf6204627e6e17bf7631769d1fdbf5a62c1a7
bosseschum <accounts@bschum.de>
2026-04-15 15:43:18 +0200

    Routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,26 @@ Rails.application.routes.draw do
   devise_for :members
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Kiosk - no login
+  namespace :kiosk do
+    root "drinks#index"
+    resources :drinks, only: [ :index, :create ]
+  end
+
+  # Kassenwart
+  namespace :treasurer do
+    root "dashboard#index"
+    resources :members
+    resources :transactions, only: [ :index, :new, :create ]
+  end
+
+  # Bierkassenwart
+  namespace :inventory do
+    root "dashboard#index"
+    resources :products
+    resources :purchases, only: [ :index, :new, :create ]
+    resources :inventory_counts, only: [ :index, :new, :create ]
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -11,5 +31,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "kiosk/drinks#index"
 end
