04f1dace7b77199cebe080dfbde0b51efd15bb2b
bosseschum <accounts@bschum.de>
2026-05-22 14:01:15 +0200

    add multi-tenancy

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,11 +1,11 @@
 class Setting < ApplicationRecord
   belongs_to :organization
 
-  def self.get(key, default = nil, organization:)
+  def self.get(key, default = nil, organization: nil)
     find_by(key: key, organization: organization)&.value || default
   end
 
-  def self.set(key, value, organization:)
+  def self.set(key, value, organization: nil)
     find_or_initialize_by(key: key, organization: organization).tap do |setting|
       setting.value = value
       setting.save
