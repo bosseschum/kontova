42c0daa4654c23fabf325b1c227e59a06c42fdb4
bosseschum <accounts@bschum.de>
2026-06-18 13:43:44 +0200

    delete member from org only; keep data history for reuse on create

--- app/controllers/treasurer/members_controller.rb
+++ app/controllers/treasurer/members_controller.rb
@@ -9,10 +9,43 @@ class Treasurer::MembersController < Treasurer::BaseController
   end
 
   def create
-    @member = Member.new(member_params)
-    @membership = @member.organization_memberships.build(
-      membership_params.merge(organization: current_organization)
-    )
+    existing_member = Member.find_by(email: member_params[:email])
+
+    if existing_member
+      @member = existing_member
+      @membership = @member.organization_memberships.build(
+        membership_params.merge(organization: current_organization)
+      )
+
+      if @member.organization_memberships.exists?(organization: current_organization)
+        @member = Member.new(member_params) # reset for error display
+        @member.errors.add(:email, "ist bereits Mitglied in diesem Verein")
+        @membership = @member.organization_memberships.build(organization: current_organization)
+        return render :new, status: :unprocessable_entity
+      end
+
+      if @membership.save
+        MemberMailer.welcome(@member, @membership.pin, nil, current_organization).deliver_later
+        redirect_to treasurer_members_path, notice: "Mitglied hinzugefügt"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    else
+      @member = Member.new(member_params)
+      @membership = @member.organization_memberships.build(
+        membership_params.merge(organization: current_organization)
+      )
+
+      if @member.save
+        plain_password = @member.generated_password || member_params[:password]
+        MemberMailer.welcome(@member, @membership.pin, plain_password, current_organization).deliver_later
+        redirect_to treasurer_members_path, notice: "Mitglied angelegt"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
 
     if @member.save
       plain_password = @member.generated_password || member_params[:password]
@@ -46,14 +79,8 @@ class Treasurer::MembersController < Treasurer::BaseController
   end
 
   def destroy
-    member = current_organization.members.find(params[:id])
     membership = current_organization.organization_memberships.find_by!(member_id: params[:id])
-
-    if member.organization_memberships.count == 1
-      member.destroy
-    else
-      membership.destroy
-    end
+    membership.destroy
     redirect_to treasurer_members_path, notice: "Mitglied gelöscht"
   end
 
